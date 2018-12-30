eb86ce50e2c6ca8774bdea8449db6cf21a4e7c5c
antoine <calmettes.antoine2@gmail.com>
2018-12-30 14:32:05 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var louerPanier = get.getElementsByClassName("louer-moi");// la variable louerPanier contient les class louer-moi
+
+
+function fuctionPanier(){
+
+}
+
+louerPanier.addEventListener("click", fuctionPanier, false);
